b7e98ac1edf27bfad6fc4fe6cdae17d23e7b270f
Gessio Mori <gessiomorin@gmail.com>
2022-11-03 09:48:41 -0300

    downgrades puppeteer

--- src/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var client_1 = require("@prisma/client");
-if (!global.prisma) {
-    global.prisma = new client_1.PrismaClient();
-}
-exports["default"] = global.prisma;
